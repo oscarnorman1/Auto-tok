c8dc61c93f6681424fa944449255828c5f55b5b4
Oscar Norman <oscar.norman95@gmail.com>
2022-11-24 17:12:06 +0100

    Sample video added

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import moviepy.editor as mpy
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+clip = mpy.VideoFileClip("assets/basevideo.mp4")
 
+txt_clip = mpy.TextClip("Sample text", fontsize = 70, color = 'white')
+txt_clip = txt_clip.set_position('center').set_duration(10)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+video = mpy.CompositeVideoClip([clip, txt_clip])
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+video.write_videofile("test.mp4")
