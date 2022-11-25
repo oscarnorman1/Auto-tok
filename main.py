48381fc2c5bfa6d0063139df746301d86c8e2f99
Oscar Norman <oscar.norman95@gmail.com>
2022-11-25 08:08:35 +0100

    testing

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import moviepy.editor as mpy
+from moviepy.video.compositing.concatenate import concatenate_videoclips
 
-clip = mpy.VideoFileClip("assets/basevideo.mp4")
+intro = mpy.VideoFileClip("assets/basevideo.mp4").subclip(0, 10)
+comment = mpy.VideoFileClip("assets/basevideo.mp4").subclip(0, 10)
 
-txt_clip = mpy.TextClip("Sample text", fontsize = 70, color = 'white')
-txt_clip = txt_clip.set_position('center').set_duration(10)
+intro_txt = mpy.TextClip("Reddit post title", fontsize=40, color='white')
+intro_txt = intro_txt.set_position('center').set_duration(5)
 
-video = mpy.CompositeVideoClip([clip, txt_clip])
+comment_txt = mpy.TextClip("Reddit post comments", fontsize=40, color='white')
+comment_txt = comment_txt.set_position('center').set_duration(5)
 
-video.write_videofile("test.mp4")
+intro_vid = mpy.CompositeVideoClip([intro, intro_txt])
+comment_vid = mpy.CompositeVideoClip([comment, comment_txt])
+
+final_clip = concatenate_videoclips([intro_vid, comment_vid])
+
+#final_clip.preview()
+final_clip.show(11, interactive=True)
+
+# video.write_videofile("test.mp4")
