18001d6dfddc5f5ebd0b806a5cffd93078544ad5
Oscar Norman <oscar.norman95@gmail.com>
2022-11-26 11:43:02 +0100

    Start work on video editing

--- main.py
+++ main.py
@@ -24,26 +24,33 @@ def seleniumstuff(subreddit):
 
 
 def videostuff(json_blob):
-    intro = mpy.VideoFileClip("assets/basevideo.mp4").subclip(0, 5)
-    comment = mpy.VideoFileClip("assets/basevideo.mp4").subclip(0, 5)
+    sentance_list = json_blob['sentence_list']
+    textclip_list = []
 
-    intro_txt = mpy.TextClip(json_blob['title'], fontsize=40, color='white')
-    intro_txt = intro_txt.set_position('center').set_duration(5)
+    for sentence in sentance_list:
+        txt_clip = mpy.TextClip(sentence, fontsize=30, color='white').set_position('center').set_duration(2)
+        textclip_list.append(txt_clip)
 
+    VIDEO = mpy.VideoFileClip("assets/pexels-ekaterina-bolovtsova.mp4")
+    # comment = mpy.VideoFileClip("assets/pexels-ekaterina-bolovtsova.mp4").subclip(0, 5)
 
-    # TODO: Add some for of for loops to iterate through len(json_blob['sentence_list']) and create
+    textclip_submission_title = mpy.TextClip(json_blob['title'], fontsize=40, color='white')\
+        .set_position('center').set_duration(5)
+    
+
+    # TODO: Add some for loops to iterate through len(json_blob['sentence_list']) and create
     # TODO: text to speech, VideoFileClip, TextClip and CompositeVideoClip and lastly concatenate them all
 
-    comment_txt = mpy.TextClip(json_blob['content'], fontsize=10, color='white')
-    comment_txt = comment_txt.set_position('center').set_duration(5)
+    # comment_txt = mpy.TextClip(str(json_blob['sentence_list']), fontsize=10, color='white')
+    # comment_txt = comment_txt.set_position('center').set_duration(5)
 
-    intro_vid = mpy.CompositeVideoClip([intro, intro_txt])
-    comment_vid = mpy.CompositeVideoClip([comment, comment_txt])
+    # intro_vid = mpy.CompositeVideoClip([intro, intro_txt])
+    # comment_vid = mpy.CompositeVideoClip([comment, comment_txt])
 
-    final_clip = concatenate_videoclips([intro_vid, comment_vid])
+    # final_clip = concatenate_videoclips([intro_vid, comment_vid])
 
     # final_clip.preview()
-    final_clip.show(7, interactive=True)
+    # final_clip.show(7, interactive=True)
     # final_clip.write_videofile("test.mp4")
 
 
@@ -61,8 +68,8 @@ def main():
     subreddit = 'TalesFromRetail'
     # seleniumstuff(subreddit)
     reddit_json_blob = fetch_reddit_stuff(subreddit)
-    print(reddit_json_blob['sentence_list'])
-    # videostuff(reddit_json_blob)
+    # print(reddit_json_blob['sentence_list'])
+    videostuff(reddit_json_blob)
 
 
 if __name__ == "__main__":
