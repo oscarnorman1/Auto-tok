5c699d72019adc633fe9f6483eebde63e3b393c1
Oscar Norman <oscar.norman95@gmail.com>
2022-11-27 01:24:14 +0100

    WIP, struggle with audio duration

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import moviepy.editor as mpy
 from selenium import webdriver
-from gtts import gTTS
+from pydub import AudioSegment
 import reddit as r
 import json
 import config
 import subredditlist
+import pyttsx3
 
 
 def seleniumstuff(subreddit):
@@ -23,37 +24,51 @@ def seleniumstuff(subreddit):
     fox.quit()
 
 
-def videostuff(json_blob, text_to_speech):
+def videostuff(json_blob):
     sentance_list = json_blob['sentence_list']
     text_clips_list = []
     final_video = None
 
-    background_clip = mpy.VideoFileClip('assets/pexels-ekaterina-bolovtsova.mp4')
+    AudioSegment.ffmpeg = 'ffmpeg/'
+    audio = AudioSegment.from_file('results/audio/test.mp3')
+    # audio_length = audio.info.length
+    # print(audio_length)
 
-    image = (mpy.ImageClip("results/img/postTitle.png").set_duration(5).set_pos('center'))
-    final = mpy.CompositeVideoClip([background_clip, image])
+    tmp1 = mpy.VideoFileClip('assets/pexels-ekaterina-bolovtsova.mp4').subclip(0, 7)
+    tmp2 = mpy.VideoFileClip('assets/pexels-ekaterina-bolovtsova.mp4')
+    tmp3 = mpy.VideoFileClip('assets/pexels-ekaterina-bolovtsova.mp4')
+    tmp4 = mpy.VideoFileClip('assets/pexels-ekaterina-bolovtsova.mp4')
 
-    #    textclip_submission_title = mpy.TextClip(json_blob['title'], fontsize=40, color='white') \
-    #        .set_position('center').set_duration(5)
+    image_original = (mpy.ImageClip("results/img/postTitle.png").set_duration(7).set_pos('center'))
+    final_intro_title_display = mpy.CompositeVideoClip([tmp1, image_original])
+    comment_display = mpy.concatenate_videoclips((tmp2, tmp3, tmp4))
+
+    # final = mpy.concatenate_videoclips((intro_title_display, tmp2, tmp3, tmp4))
 
     for sentence in sentance_list:
-        text_clip = mpy.TextClip(sentence, fontsize=30, color='white').set_position('center').set_duration(2)
+        text_clip = mpy.TextClip(sentence, font="Arial", fontsize=75, color='white').set_position(
+            'center').set_duration(3)
         text_clips_list.append(text_clip)
 
-    final_text_clip = mpy.concatenate_videoclips(text_clips_list)
-    final = mpy.CompositeVideoClip([final, final_text_clip])
-    final.resize(height=1920, width=1080)
-    final.write_videofile('result.mp4')
-
-
+    final_text_clip = mpy.concatenate_videoclips(text_clips_list).set_position('center')
+    final_comment_display = mpy.CompositeVideoClip([comment_display, final_text_clip])
+    final = mpy.concatenate_videoclips([final_intro_title_display, final_comment_display])
+    final.resize(height=1280, width=720)
+    final.show(17, interactive=True)
+    # final.write_videofile('result.mp4', threads=8, fps=30)
 
     # TODO: Add some for loops to iterate through len(json_blob['sentence_list']) and create
     # TODO: text to speech, VideoFileClip, TextClip and CompositeVideoClip and lastly concatenate them all
 
 
 def text_to_speech_stuff(text):
-    audio = gTTS(text=text, lang='en', tld='co.uk', slow=False)
-    audio.save()
+    # audio = gTTS(text=text, lang='en', tld='co.uk', slow=False)
+    # audio.save("results/audio/text_to_speech_content.mp3")
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', 'com.apple.speech.synthesis.voice.Alex')
+    engine.save_to_file(text, 'results/audio/test.mp3')
+    engine.runAndWait()
 
 
 def fetch_reddit_stuff(subreddit):
