f49f7bc8476ed2eb255108d2ab464f7f80b845c4
oscarnorman1 <oscar.norman95@gmail.com>
2022-11-28 23:12:30 +0100

    Frustration is on top!

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def seleniumstuff(subreddit):
     fox.quit()
 
 
-def videostuff(json_blob, audio_durations_array):
-    sentance_list = json_blob['sentence_list']
+def video_stuff(json_blob, audio_durations_array):
+    sentence_list = json_blob['sentence_list']
     text_clips_list = []
 
     audio_title_duration = audio_durations_array[0]
@@ -36,35 +36,50 @@ def videostuff(json_blob, audio_durations_array):
     print(f'title dur: {audio_title_duration}')
     print(f'content dur: {audio_content_duration}')
 
-    tmp1 = mpy.VideoFileClip('assets/pexels-ekaterina-bolovtsova.mp4')
-    tmp2 = mpy.VideoFileClip('assets/pexels-ekaterina-bolovtsova.mp4')
-    tmp3 = mpy.VideoFileClip('assets/pexels-ekaterina-bolovtsova.mp4')
-    tmp4 = mpy.VideoFileClip('assets/pexels-ekaterina-bolovtsova.mp4')
-
+    # Image to add to title video
     image_original = (mpy.ImageClip("results/img/postTitle.png").set_duration(7).set_pos('center'))
-    final_intro_title_display = mpy.CompositeVideoClip([tmp1, image_original])
-    comment_display = mpy.concatenate_videoclips((tmp2, tmp3, tmp4))
 
-    # final = mpy.concatenate_videoclips((intro_title_display, tmp2, tmp3, tmp4))
+    # Audio to title video
+    intro_title_audio = mpy.AudioFileClip('results/audio/test_title.mp3')
 
-    for sentence in sentance_list:
+    # Intro title video
+    intro_title_display = get_concatenated_background_video(3)
+    final_intro_title_display = mpy.CompositeVideoClip([intro_title_display, image_original]) \
+        .subclip(0, audio_title_duration + 2)
+    final_intro_title_display.set_audio(intro_title_audio)
+    print(f'intro vid duration: {final_intro_title_display.duration}')
+
+    # Content title video
+    comment_display = get_concatenated_background_video(15)
+    for sentence in sentence_list:
         text_clip = mpy.TextClip(sentence, font="Arial", fontsize=45, color='white').set_position(
             'center').set_duration(3)
         text_clips_list.append(text_clip)
-
     final_text_clip = mpy.concatenate_videoclips(text_clips_list).set_position('center')
-    final_comment_display = mpy.CompositeVideoClip([comment_display, final_text_clip])
-    final = mpy.concatenate_videoclips([final_intro_title_display, final_comment_display])
-    final.resize((720, 1280))
-    final.show(2, interactive=True)
-    # final.write_videofile('result.mp4', threads=8, fps=30)
+    final_comment_display = mpy.CompositeVideoClip([comment_display, final_text_clip])\
+        .subclip(0, audio_content_duration)
 
+    # Final video
+    final = mpy.concatenate_videoclips([final_intro_title_display, final_comment_display])
+
+    # Preview or write
+    final.resize((720, 1280))
+    # final.show(2, interactive=True)
+    final.write_videofile('result.mp4', threads=12, fps=30)
+
+
+def get_concatenated_background_video(n):
+    tmp = []
+    for x in range(n):
+        tmp.append(mpy.VideoFileClip('assets/pexels-ekaterina-bolovtsova.mp4'))
+    return mpy.concatenate_videoclips(tmp, method='compose')
 
 
 def text_to_speech_stuff(text_array):
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[0])
+    engine.setProperty('rate', 150)
     engine.save_to_file(text_array[0], 'results/audio/test_title.mp3')
     engine.save_to_file(text_array[1], 'results/audio/test_content.mp3')
     engine.runAndWait()
@@ -74,6 +89,7 @@ def text_to_speech_stuff(text_array):
 
     return [math.ceil(audio_title.duration_seconds), math.ceil(audio_content.duration_seconds)]
 
+
 def fetch_reddit_stuff(subreddit):
     reddit = r.return_instance()
     blob = json.loads(r.return_blob(subreddit, reddit))
@@ -81,12 +97,12 @@ def fetch_reddit_stuff(subreddit):
 
 
 def main():
-    subreddit = subredditlist.getRandomSub()
-    # subreddit = 'TrueOffMyChest'
+    # subreddit = subredditlist.getRandomSub()
+    subreddit = 'TrueOffMyChest'
     reddit_json_blob = fetch_reddit_stuff(subreddit)
-    # seleniumstuff(subreddit)
+    seleniumstuff(subreddit)
     audio_durations = text_to_speech_stuff([reddit_json_blob['title'], reddit_json_blob['sentence_list_full']])
-    videostuff(reddit_json_blob, audio_durations)
+    video_stuff(reddit_json_blob, audio_durations)
 
 
 if __name__ == "__main__":
